07d94f76e8ef3f0f10719ff829e659172e4c3332
felocSsS <71203042+felocSsS@users.noreply.github.com>
2021-10-19 13:59:29 +0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,33 @@
+<html>
+    <head>
+        <link rel="preconnect" href="https://fonts.googleapis.com">
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+        <link href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,300;1,400;1,500;1,700;1,900&display=swap" rel="stylesheet">
+        <link rel="stylesheet" href="./css/style.css"/>
+        <title>
+            Кукарача
+        </title>
+    </head>
+    <body class="bd">
+        <div class="navMenu">
+            <ul class="nav3">
+                
+                <li><a href="https://vk.com/vlad__shipilov">Home</a>
+                <li><a href="#2">Запрещенные комиксы</a>
+                <li><a href="#3">Аниме комиксы</a>
+                <li><a href="#4">Справка</a>
+            </ul>
+		</div>
+
+        <div class="text">
+            Добро пожаловать на сайт FAKE комиксов
+        </div>
+
+        <div class="text">
+            Здесь вы найдете комиксы про ♂fucking slave♂ и их хозяина ♂Danger Master♂
+        </div>
+       <?php
+            
+       ?> 
+    </body>
+</html>
